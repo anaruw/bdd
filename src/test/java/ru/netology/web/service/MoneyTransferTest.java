719c5d8e0fac0d18646e28429f0fe03245d1c421
Andrey Kononenko <anaruw_13@mail.ru>
2025-03-16 13:14:00 +0300

    class-files added

---
+++ src/test/java/ru/netology/web/service/MoneyTransferTest.java
@@ -0,0 +1,95 @@
+package ru.netology.web.service;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.TransferInfo;
+import ru.netology.web.data.UserInfo;
+import ru.netology.web.data.VerificationCode;
+import ru.netology.web.pages.DashBoardPage;
+import ru.netology.web.pages.LoginPage;
+import ru.netology.web.pages.TransferPage;
+import ru.netology.web.pages.VerificationPage;
+import ru.netology.web.util.DataHelper;
+
+public class MoneyTransferTest {
+
+    @BeforeEach
+    public void setUp() {
+        Selenide.open("http://localhost:9999");
+    }
+
+    @Test
+    public void validTransferTest() {
+
+        String cardNumberFrom = "5559 0000 0000 0001";
+        String cardNumberTo = "5559 0000 0000 0002";
+        int amount = 1000;
+
+        UserInfo info = DataHelper.getAuthInfo();
+        VerificationCode verificationCode = DataHelper.newVerificationCode(info);
+        TransferInfo planningTransfer = DataHelper.planningTransfer(cardNumberFrom, cardNumberTo, amount);
+
+        LoginPage loginPage = new LoginPage();
+
+        VerificationPage verificationPage = loginPage.validAuth(info);
+        DashBoardPage dashBoardMain = verificationPage.validVerify(verificationCode);
+
+        int fromInitialBalance = DataHelper.cardBalance(dashBoardMain.cardChoice(cardNumberFrom));
+        int toInitialBalance = DataHelper.cardBalance(dashBoardMain.cardChoice(cardNumberTo));
+
+        TransferPage testTransfer = dashBoardMain.replenishment(planningTransfer);
+        dashBoardMain = testTransfer.validTransfer(planningTransfer);
+
+        dashBoardMain.resultOfValidReplenishment(planningTransfer, fromInitialBalance, toInitialBalance);
+    }
+
+    @Test
+    public void canceledTransferWithCompletedInput() {
+        String cardNumberFrom = "5559 0000 0000 0001";
+        String cardNumberTo = "5559 0000 0000 0002";
+        int amount = 10;
+
+        UserInfo info = DataHelper.getAuthInfo();
+        VerificationCode verificationCode = DataHelper.newVerificationCode(info);
+        TransferInfo planningTransfer = DataHelper.planningTransfer(cardNumberFrom, cardNumberTo, amount);
+
+        LoginPage loginPage = new LoginPage();
+
+        VerificationPage verificationPage = loginPage.validAuth(info);
+        DashBoardPage dashBoardMain = verificationPage.validVerify(verificationCode);
+
+        int fromInitialBalance = DataHelper.cardBalance(dashBoardMain.cardChoice(cardNumberFrom));
+        int toInitialBalance = DataHelper.cardBalance(dashBoardMain.cardChoice(cardNumberTo));
+
+        TransferPage testTransfer = dashBoardMain.replenishment(planningTransfer);
+        dashBoardMain = testTransfer.canceledTransferWithCompletedInput(planningTransfer);
+
+        dashBoardMain.resultOfInvalidReplenishment(planningTransfer, fromInitialBalance, toInitialBalance);
+    }
+
+    @Test
+    public void transferWithEmptyCardFromInput() {
+        String cardNumberFrom = "5559 0000 0000 0001";
+        String cardNumberTo = "5559 0000 0000 0002";
+        int amount = 11001;
+
+        UserInfo info = DataHelper.getAuthInfo();
+        VerificationCode verificationCode = DataHelper.newVerificationCode(info);
+        TransferInfo planningTransfer = DataHelper.planningTransfer(cardNumberFrom, cardNumberTo, amount);
+
+        LoginPage loginPage = new LoginPage();
+
+        VerificationPage verificationPage = loginPage.validAuth(info);
+        DashBoardPage dashBoardMain = verificationPage.validVerify(verificationCode);
+
+        int fromInitialBalance = DataHelper.cardBalance(dashBoardMain.cardChoice(cardNumberFrom));
+        int toInitialBalance = DataHelper.cardBalance(dashBoardMain.cardChoice(cardNumberTo));
+
+        TransferPage testTransfer = dashBoardMain.replenishment(planningTransfer);
+        testTransfer.transferWithEmptyCardFromField(planningTransfer);
+        dashBoardMain = testTransfer.canceledTransfer();
+
+        dashBoardMain.resultOfInvalidReplenishment(planningTransfer, fromInitialBalance, toInitialBalance);
+    }
+}
